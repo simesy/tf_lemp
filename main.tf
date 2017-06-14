9ebbf35bb7ab060eb9e1f284b0d80a39180ae54c
Si Hobbs <simon@hobbs.id.au>
2017-06-14 14:23:42 +1000

    Moved the code around a bit.

--- main.tf
+++ main.tf
@@ -1,10 +1,18 @@
-# Specify the provider and access details
+#
+# Define a Load Balancer and Autoscaling group of nginx servers.
+#
 
 provider "aws" {
   region = "${var.aws_region}"
 }
 
-# User data script for webserver.
+
+#
+# Prepping launch configuration, ssh keys and security groups.
+#
+
+# Get the user_data script that will run on each nginx server.
+# Used in aws_launch_configuration.
 resource "template_file" "user_data_nginx" {
   template =  "${file("${path.module}/nginx/user_data.tpl")}"
   vars {
@@ -16,12 +24,96 @@ resource "template_file" "user_data_nginx" {
   }
 }
 
-# SSH Key for remote access.
+# SSH Key for remote access to nginx servers.
+# Used in aws_launch_configuration.
 resource "aws_key_pair" "ssh_key" {
   key_name   = "ssh-access"
   public_key = "${var.remote_key}"
 }
 
+# AWS Launch configuration for auto scaling group.
+resource "aws_launch_configuration" "lc_prod" {
+  image_id      = "${var.aws_ami}"
+  instance_type = "${var.aws_size}"
+  key_name      = "ssh-access"
+  # Security group
+  security_groups = ["${aws_security_group.sg_default.id}"]
+  user_data       = "${template_file.user_data_nginx.rendered}"
+}
+
+# The security group allows HTTP in and everything out.
+resource "aws_security_group" "sg_default" {
+  name        = "${var.identifier}-web-sg"
+  description = "Http and optionally SSH traffic."
+
+  # HTTP access from anywhere.
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound internet access.
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    "Name" = "${var.identifier}-web-sg"
+    "Application ID" = "${var.tag}"
+  }
+}
+
+# Attach an SSH access rule  to the security group if remote_access=true
+resource "aws_security_group_rule" "sg_rule_allow_ssh" {
+  type            = "ingress"
+  from_port       = 22
+  to_port         = 22
+  protocol        = "tcp"
+  cidr_blocks     = ["0.0.0.0/0"]
+
+  security_group_id = "${aws_security_group.sg_default.id}"
+
+  # This logic determines if 0 (disabled) or 1 (enabled) SSH inbound
+  # rules are added to the security group.
+  count = "${var.remote_access == "true" ? 1 : 0}"
+}
+
+
+#
+# Autoscaling Group and Load Balancer.
+#
+
+# AWS Autoscaling group
+resource "aws_autoscaling_group" "asg_prod" {
+  availability_zones   = ["${split(",", var.aws_az)}"]
+  name                 = "${var.identifier}-web-asg"
+  max_size             = "${var.asg_max}"
+  min_size             = "${var.asg_min}"
+  desired_capacity     = "${var.asg_desired}"
+  force_delete         = true
+  launch_configuration = "${aws_launch_configuration.lc_prod.name}"
+  load_balancers       = ["${aws_elb.elb_prod.name}"]
+
+  tag {
+    key                 = "Name"
+    value               = "${var.identifier}-web-asg"
+    propagate_at_launch = "true"
+  }
+
+  tag {
+    key                 = "Application ID"
+    value               = "${var.tag}"
+    propagate_at_launch = "true"
+  }
+}
+
+
+# AWS Load balancer
 resource "aws_elb" "elb_prod" {
   name = "${var.identifier}-web-elb"
 
@@ -49,78 +141,4 @@ resource "aws_elb" "elb_prod" {
     "Application ID" = "${var.tag}"
   }
 
-}
-
-resource "aws_autoscaling_group" "asg_prod" {
-  availability_zones   = ["${split(",", var.aws_az)}"]
-  name                 = "${var.identifier}-web-asg"
-  max_size             = "${var.asg_max}"
-  min_size             = "${var.asg_min}"
-  desired_capacity     = "${var.asg_desired}"
-  force_delete         = true
-  launch_configuration = "${aws_launch_configuration.lc_prod.name}"
-  load_balancers       = ["${aws_elb.elb_prod.name}"]
-
-  tag {
-    key                 = "Name"
-    value               = "${var.identifier}-web-asg"
-    propagate_at_launch = "true"
-  }
-
-  tag {
-    key                 = "Application ID"
-    value               = "${var.tag}"
-    propagate_at_launch = "true"
-  }
-}
-
-resource "aws_launch_configuration" "lc_prod" {
-  image_id      = "${var.aws_ami}"
-  instance_type = "${var.aws_size}"
-  key_name      = "ssh-access"
-  # Security group
-  security_groups = ["${aws_security_group.sg_default.id}"]
-  user_data       = "${template_file.user_data_nginx.rendered}"
-}
-
-# Our default security group to access
-# the instances over SSH and HTTP
-resource "aws_security_group" "sg_default" {
-  name        = "${var.identifier}-web-sg"
-  description = "Http and optionally SSH traffic."
-
-  # HTTP access from anywhere.
-  # @todo pin down to ELB only.
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Outbound internet access.
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags {
-  //  "Name" = "${format("%s-web-sg", "${var.identifier}")}"
-    "Name" = "${var.identifier}-web-sg"
-    "Application ID" = "${var.tag}"
-  }
-}
-
-resource "aws_security_group_rule" "sg_rule_allow_ssh" {
-  type            = "ingress"
-  from_port       = 22
-  to_port         = 22
-  protocol        = "tcp"
-  cidr_blocks     = ["0.0.0.0/0"]
-
-  security_group_id = "${aws_security_group.sg_default.id}"
-
-  count = "${var.remote_access == "true" ? 1 : 0}"
 }
