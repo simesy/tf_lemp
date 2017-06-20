f81516f608961251095865066881b548a55cf210
Si Hobbs <simon@hobbs.id.au>
2017-06-20 10:53:12 +1000

    Ensure the private key permissions are correct for SSH.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,15 +5,20 @@ if [ -f "terraform.tfstate" ]; then
    exit
 fi
 
+# Deploy to AWS.
 terraform apply -var-file ./tests/spec/test.tfvar
 
 echo "Wait 2 minutes to allow an ASG instance to come up."
 sleep 120
 terraform apply -var-file ./tests/spec/test.tfvar
 
+# Ensure correct permission of private key.
+chmod 600 tests/spec/insecure_key
+
+# Run tests (see tests/spec/default_spec.rb)
 cd tests
 bundle exec rake spec
 cd ..
 
+# Clean up.
 terraform destroy -force && rm terraform.*
-
